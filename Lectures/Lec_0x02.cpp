d3303c1e75f54ae17f48ab7ee869abf0f9e08c6a
bokdol11859 <2019147551@yonsei.ac.kr>
2022-07-05 22:02:14 +0900

    C++ stl

---
+++ Lectures/Lec_0x02.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool cmp2(vector<int>& v1, vector<int>& v2, int idx){
+    return v1[idx] > v2[idx];
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+//    vector<int> v1(100);
+//    vector<int> v2(100);
+//
+//    string s;
+//    getline(cin, s);
+//
+//    cout << s;
+//
+//    v1[4] = 5;
+//    v2[4] = 3;
+//
+//    cout << cmp2(v1, v2, 4);
+
+    int x, y;
+    cin >> x >> y;
+    for(int i = 0; i < x; i++){
+        int temp;
+        cin >> temp;
+        if(temp < y) cout << temp << ' ';
+    }
+}
