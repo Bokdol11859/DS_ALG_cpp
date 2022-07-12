12bce33b079de3b0458c07c5755c47d543e1b117
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2022-07-12 14:48:52 +0900

    BOJ 1021

--- BOJ/1021.cpp
+++ BOJ/1021.cpp
@@ -1,7 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int find(deque<int> dq, int num){
+    auto itr = find(dq.begin(), dq.end(), num);
+    if(itr != dq.end()){
+        return itr - dq.begin();
+    } else {
+        return -1;
+    }
+}
+
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
@@ -11,21 +20,45 @@ int main(){
     int n, m;
     cin >> n >> m;
 
+    for(int i = 1; i <= n; i++){
+        DQ.push_back(i);
+    }
+
     int nums[m];
     for(int i = 0; i < m; i++){
         cin >> nums[i];
     }
-    int lefts[m];
-    int rights[m];
+
+    int mid = n / 2;
+
     int count = 0;
-    for(int a : nums){
-        lefts[count] = a - 1;
-        rights[count] = n - a;
+
+    for(int i = 0; i < m; i++){
+        int idx = find(DQ, nums[i]);
+        int right = DQ.size() - idx;
+        int left = idx;
+        if(right < left){
+            for(int j = 0; j < right; j++){
+                DQ.push_front(DQ.back());
+                DQ.pop_back();
+                count++;
+            }
+            DQ.pop_front();
+        } else if(left < right){
+            for(int j = 0; j < left; j++){
+                DQ.push_back(DQ.front());
+                DQ.pop_front();
+                count++;
+            }
+            DQ.pop_front();
+        } else {
+            for(int j = 0; j < left; j++){
+                DQ.push_back(DQ.front());
+                DQ.pop_front();
+                count++;
+            }
+            DQ.pop_front();
+        }
     }
-
-    for(int i = 0; i < n; i++){
-
-    }
-
-
+    cout << count;
 }
